edbc47ebc6585368ef1626405bc32b11316e1097
Riccardo Remigio <riccardoremigio@gmail.com>
2018-03-19 18:11:31 +0100

    controllerModule.c: Implemented "controller_thread" function
    "controller_thread" loops and print a message every 100ms until the module is removed.
    "controller_module_init" creates the thread.
    "controller_module_exit" sets the condition to stop the thread.

--- controllerModule.c
+++ controllerModule.c
@@ -1,17 +1,48 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/kthread.h>
+#include <linux/delay.h>
 
-static int __init hello_init(void){
-	printk(KERN_INFO "Hello World!\n");
+static struct task_struct *thread = NULL;
+
+/*
+ *this function runs a loop that prints a message (for the moment)
+ *
+ */
+static int controller_thread(void *data){
+	while(!kthread_should_stop()){
+		printk(KERN_INFO "I'm running!\n");
+		msleep(100);
+	}
+	printk(KERN_INFO "Thread end\n");
 	return 0;
 }
 
-static void __exit hello_exit(void){
-	printk(KERN_INFO "Goodbye World!\n");
+/*
+ *Init function called at module insertion, it creates a thread
+ *that runs the controller_thread function, then it returns.
+ *
+ */
+static int __init controller_module_init(void){
+	printk(KERN_INFO "Thread started\n");
+	thread = kthread_run(controller_thread, NULL, "control_module_thread");
+	return 0;
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+/*
+ *Exit function called when the module is removed, it stops the main
+ *loop in controller_thread and returns.
+ *
+ */
+static void __exit controller_module_exit(void){
+	kthread_stop(thread);
+	printk(KERN_INFO "Module exit\n");
+}
+
+module_init(controller_module_init);
+module_exit(controller_module_exit);
+
+
 
 MODULE_LICENSE("GPL");
