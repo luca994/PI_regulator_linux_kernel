ab9d8ab7a019b884617f761a45d3f3dede7b475d
Riccardo Remigio <riccardoremigio@gmail.com>
2018-03-25 11:51:47 +0200

    controllerModule.c : substituted define with masks in msr-index.h, modified msr functions
    msr functions write/read on single cores instead of all the cores, thread_controller loops on all cores and
    calls msr functions for each core

--- controllerModule.c
+++ controllerModule.c
@@ -4,18 +4,14 @@
 #include <linux/kthread.h>
 #include <linux/delay.h>
 #include <asm/msr.h>
+#include <asm/msr-index.h>
 #include <linux/kfifo.h>
 #include <linux/sched.h>
 
-#define TCC_CELSIUS 100
 #define LOGICAL_CORES_N 8
-#define IA32_THERM_STATUS 0x19c
-#define MSR_TEMPERATURE_TARGET 0x1a2
-#define IA32_PERF_CTL 0x199
-#define IA32_PERF_STATUS 0x198
-#define IA32_MISC_ENABLE 0x1a0
 #define TIMER 1000
 #define PRINTK_QUEUE_SIZE 512
+#define INTEL_PERF_STATUS_MASK 0xffff
 
 static struct task_struct *controller_thread = NULL;
 static struct task_struct *printk_thread = NULL;
@@ -23,7 +19,7 @@ static struct task_struct *printk_thread = NULL;
 typedef struct values_queue {
 	int core;
 	int temp;
-	int mul;
+	int mult_freq;
 } values_queue;
 
 static bool full_queue;
@@ -34,68 +30,65 @@ DEFINE_KFIFO(printk_queue, values_queue, PRINTK_QUEUE_SIZE);
 
 
 /*
- * reads the temperature from the processor register IA32_THERM_STATUS
+ * reads the temperature from the processor register MSR_IA32_THERM_STATUS
  * using rdmsr function, then returns it
  */
-static int get_temperature_msr(void){
-	int tjunction_celsius;
+static int get_temperature_msr(unsigned int core){
+	unsigned int tjunction_celsius;
     u32 val, dummy;
-    rdmsr(MSR_TEMPERATURE_TARGET,val,dummy);
+    rdmsr(MSR_IA32_TEMPERATURE_TARGET,val,dummy);
     tjunction_celsius = ((val&(0xff<<16))>>16);
-	rdmsr(IA32_THERM_STATUS, val, dummy);
-	val = val & 0x7F0000;
-	val = val >> 16;
+	rdmsr_on_cpu(core,MSR_IA32_THERM_STATUS, &val, &dummy);
+	val = (val&THERM_MASK_THRESHOLD1) >> THERM_SHIFT_THRESHOLD1;
 	val = tjunction_celsius - val;
 	return val;
 }
 
 /*
  * this function reads the current value for the frequency in the msr register
- * called IA32_PERF_STATUS
+ * called MSR_IA32_PERF_STATUS
  */
-static int read_frequency_msr(void){
+static int read_frequency_msr(unsigned int core){
 	u32 val, dummy;
-	rdmsr(IA32_PERF_STATUS, val, dummy);
-	val &= 0xFFFF;
+	rdmsr_on_cpu(core,MSR_IA32_PERF_STATUS, &val, &dummy);
+	val &= INTEL_PERF_STATUS_MASK;
 	return (val >> 8);
 }
 
 /*
  * this function disbles the bit related to TCC in msr register
- * called IA32_MISC_ENABLE
+ * called MSR_IA32_MISC_ENABLE
  */
 static void disable_tcc(void){
     u64 val, dummy;
-    rdmsr(IA32_MISC_ENABLE, val, dummy);
+    rdmsr(MSR_IA32_MISC_ENABLE, val, dummy);
     val &= ~(1UL<<3); 
-    wrmsr(IA32_MISC_ENABLE, val, dummy);
+    wrmsr(MSR_IA32_MISC_ENABLE, val, dummy);
 }
 
 /*
  * this function enables the bit related to TCC in msr register
- * called IA32_MISC_ENABLE
+ * called MSR_IA32_MISC_ENABLE
  */
 static void enable_tcc(void){
     u64 val, dummy;
-    rdmsr(IA32_MISC_ENABLE, val, dummy);
+    rdmsr(MSR_IA32_MISC_ENABLE, val, dummy);
     val |= (1UL<<3);
-    wrmsr(IA32_MISC_ENABLE, val, dummy);
+    wrmsr(MSR_IA32_MISC_ENABLE, val, dummy);
 }
 
 /*
  * this function writes the new frequency in the msr register called
- * IA32_PERF_CTL in all the cpu cores
+ * MSR_IA32_PERF_CTL in all the cpu cores
  * 
  */
-static void write_frequency_msr(u64 mult_freq){
-	u64 val, dummy;
-    int i,j;
-    rdmsr(IA32_PERF_CTL, val, dummy);
-    for(i=8;i<16;i++)
-        val &= ~(1UL<<i);
-    val |= (mult_freq<<8);
-    for(j=0;j<LOGICAL_CORES_N;j++)
-    	wrmsr_on_cpu(j,IA32_PERF_CTL, val, dummy);
+static void write_frequency_msr(unsigned int mult_freq,unsigned int core){
+	u32 val, dummy;
+    //TODO check the frequency before write (control if it is too high)
+    rdmsr_on_cpu(core, MSR_IA32_PERF_CTL, &val, &dummy);
+    val &= INTEL_PERF_CTL_MASK<<16;
+    val |= (mult_freq<<8);    
+    wrmsr_on_cpu(core, MSR_IA32_PERF_CTL, val, dummy);
 }
 
 /*
@@ -112,10 +105,11 @@ static int thread_printk(void *data){
         	schedule();
   		}
 		kfifo_out(&printk_queue, &values, 1);
-		printk(KERN_INFO "Frequency Multiplier = %i\nTemperature = %i°C\n", values.mul, values.temp);
+		printk(KERN_INFO "Core:%i\nFrequency Multiplier = %i\nTemperature = %i°C\n-------------------------\n", values.core, values.mult_freq, values.temp);
 		if(full_queue)
 			printk(KERN_WARNING "The queue of log is full");
 	}
+	return 0;
 }
 
 /*
@@ -124,28 +118,33 @@ static int thread_printk(void *data){
  *
  */
 static int thread_controller(void *data){
-    u64 val, dummy;
-    u64 mul = freq_param;
+    int count_cores;
+    unsigned int mult_freq = freq_param;
     values_queue values;
     //disable_tcc();
-    write_frequency_msr(mul);
-    printk(KERN_INFO "Setting freq to: %i", mul);
+    
+    printk(KERN_INFO "Setting freq to: %i", mult_freq);
 	while(!kthread_should_stop()){
-		values.temp=get_temperature_msr();		
-		values.mul=read_frequency_msr();
-		
-		//alternative method to print the messages (used for debugging, it should takes tenth of microseconds). 
-		//You have to read the file /sys/kernel/debug/tracing/options/trace.
-		//trace_printk("Frequency Multiplier = %i\nTemperature = %i°C\n", values.mul, values.temp);
-		
-		if(!kfifo_is_full(&printk_queue)){
-			full_queue=false;
-			kfifo_in(&printk_queue, &values, 1);
+
+		for(count_cores=0; count_cores<LOGICAL_CORES_N; count_cores++){
+			values.core=count_cores;
+			write_frequency_msr(mult_freq, count_cores);
+			values.temp=get_temperature_msr(count_cores);		
+			values.mult_freq=read_frequency_msr(count_cores);
+			
+			//alternative method to print the messages (used for debugging, it should takes tenth of microseconds). 
+			//You have to read the file /sys/kernel/debug/tracing/options/trace.
+			//trace_printk("Core:%i\nFrequency Multiplier = %i\nTemperature = %i°C\n", values.core, values.mult_freq, values.temp);
+			
+			if(!kfifo_is_full(&printk_queue)){
+				full_queue=false;
+				kfifo_in(&printk_queue, &values, 1);
+			}
+			else{
+				full_queue=true;	
+			}		
+			wake_up_process(printk_thread);
 		}
-		else{
-			full_queue=true;	
-		}		
-		wake_up_process(printk_thread);
 
 		msleep(TIMER);
 	}
