822744b8e4b71017d4968f59cb55f7e037c04cd4
Luca Santini <luca2.santini@mail.polimi.it>
2018-04-17 11:56:39 +0200

    renamed trace_msr_write

---
+++ trace_msr_write.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# use sudo to execute this script
+
+read -p 'Choose the interval tracing time: ' interv
+echo
+echo > /sys/kernel/debug/tracing/trace
+echo 1 > /sys/kernel/debug/tracing/events/msr/write_msr/enable
+sleep $interv
+cat /sys/kernel/debug/tracing/trace | grep 'write_msr: 199'
+echo 0 > /sys/kernel/debug/tracing/events/msr/write_msr/enable
