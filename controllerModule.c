7dc55d3090c1cce738fe7af92807e81fcd5ea8e3
root <root@luca-debian.homenet.telecomitalia.it>
2018-04-17 12:03:02 +0200

    controllerModule.c removed the printk fifo that is no more useful

--- controllerModule.c
+++ controllerModule.c
@@ -5,28 +5,15 @@
 #include <linux/delay.h>
 #include <asm/msr.h>
 #include <asm/msr-index.h>
-#include <linux/kfifo.h>
 #include <linux/sched.h>
 
 #define LOGICAL_CORES_N 8
-#define TIMER 1000
-#define PRINTK_QUEUE_SIZE 512
 #define INTEL_PERF_STATUS_MASK 0xffff
 
 static struct task_struct *controller_thread = NULL;
-static struct task_struct *printk_thread = NULL;
 
-typedef struct values_queue {
-	int core;
-	int temp;
-	int mult_freq;
-} values_queue;
-
-static bool full_queue;
 static int freq_param = 8;
 module_param(freq_param, int, S_IWUSR);
-
-DEFINE_KFIFO(printk_queue, values_queue, PRINTK_QUEUE_SIZE);
 
 
 /*
@@ -97,19 +84,6 @@ static void write_frequency_msr(unsigned int mult_freq,unsigned int core){
  * If the queue is full prints a warning message.
  * 
  */
-static int thread_printk(void *data){
-	values_queue values={-1,-1,-1};
-	while(!kthread_should_stop()){
-		set_current_state(TASK_UNINTERRUPTIBLE);
-  		if(kfifo_is_empty(&printk_queue)) {
-        	schedule();
-  		}
-		kfifo_out(&printk_queue, &values, 1);
-		printk(KERN_INFO "Core:%i\nFrequency Multiplier = %i\nTemperature = %i°C\n-------------------------\n", values.core, values.mult_freq, values.temp);
-		if(full_queue)
-			printk(KERN_WARNING "The queue of log is full");
-	}
-	return 0;
 }
 
 /*
@@ -118,34 +92,9 @@ static int thread_printk(void *data){
  *
  */
 static int thread_controller(void *data){
-    int count_cores;
-    unsigned int mult_freq = freq_param;
-    values_queue values;
     //disable_tcc();
     
-    printk(KERN_INFO "Setting freq to: %i", mult_freq);
 	while(!kthread_should_stop()){
-
-		for(count_cores=0; count_cores<LOGICAL_CORES_N; count_cores++){
-			values.core=count_cores;
-			write_frequency_msr(mult_freq, count_cores);
-			values.temp=get_temperature_msr(count_cores);		
-			values.mult_freq=read_frequency_msr(count_cores);
-			
-			//alternative method to print the messages (used for debugging, it should takes tenth of microseconds). 
-			//You have to read the file /sys/kernel/debug/tracing/options/trace.
-			//trace_printk("Core:%i\nFrequency Multiplier = %i\nTemperature = %i°C\n", values.core, values.mult_freq, values.temp);
-			
-			if(!kfifo_is_full(&printk_queue)){
-				full_queue=false;
-				kfifo_in(&printk_queue, &values, 1);
-			}
-			else{
-				full_queue=true;	
-			}		
-			wake_up_process(printk_thread);
-		}
-
 		msleep(TIMER);
 	}
 	//enable_tcc();
@@ -159,7 +108,6 @@ static int thread_controller(void *data){
  */
 static int __init controller_module_init(void){
 	printk(KERN_INFO "Module init\n");
-	printk_thread = kthread_run(thread_printk, NULL, "printk_thread");
 	controller_thread = kthread_run(thread_controller, NULL, "control_module_thread");
 	return 0;
 }
@@ -171,7 +119,6 @@ static int __init controller_module_init(void){
  */
 static void __exit controller_module_exit(void){
 	kthread_stop(controller_thread);
-	kthread_stop(printk_thread);
 	printk(KERN_INFO "Module exit\n");
 }
 
