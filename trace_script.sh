32bd2904415968134c55eb55db6de32f95d6337c
root <root@luca-debian.homenet.telecomitalia.it>
2018-04-19 18:22:39 +0200

    trace_script: commented the part of the script that changes the options

--- trace_script.sh
+++ trace_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for FILE in /sys/kernel/debug/tracing/options/*
-do
-    echo '0' > "${FILE}"
-done
-echo '1' > /sys/kernel/debug/tracing/options/trace_printk
+#for FILE in /sys/kernel/debug/tracing/options/*
+#do
+#    echo '0' > "${FILE}"
+#done
+#echo '1' > /sys/kernel/debug/tracing/options/trace_printk
 
 echo ' ' > /sys/kernel/debug/tracing/trace
